192bed03a3eb7f97202ebc420596a840b8fec584
iamdhariot <iamdhariot@gmail.com>
2020-07-05 17:32:45 +0530

    Initial commit

---
+++ src/evaluateGuess.kt
@@ -0,0 +1,59 @@
+package mastermind
+
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+ //println(secret)// for checking
+    val rightPosition = getGuessInRightPosition(secret,guess)
+    val wrongPosition = getGuessInWrongPosition(secret,guess)
+
+ return Evaluation(rightPosition,wrongPosition)
+}
+
+fun getGuessInRightPosition(secret: String, guess: String): Int {
+
+    var rightPosition = 0
+    for(i in 0 until secret.length){
+        if(secret[i]==guess[i]){
+            rightPosition++  // if guess and secret letters position is same then count it as a correct guess
+
+        }
+    }
+    return rightPosition
+}
+
+fun getGuessInWrongPosition(secret: String, guess: String): Int {
+    var wrongPosition=0
+    var newSecret=""
+    var newGuess=""
+    for(i in 0 until secret.length){
+        if(secret[i]!=guess[i]){
+          newSecret += secret[i]  // if guess and secret letters position is not same or they are not matched on same position then store guess and secret string in newGuess newSecret
+          newGuess+=guess[i]
+        }
+    }
+    val evaluatedChar = mutableListOf<Char>()
+    if(!newSecret.isEmpty()){
+        for(i in 0 until guess.length){
+            var char = guess[i] // taking one char/ letter at a time for evaluation
+            // check this char is evaluated or not
+            if(!evaluatedChar.contains(char)){
+                val totalCharMatchedInSecret = countMatched(newSecret,char) // total number of char matched in newSecret
+                val totalCharMatchedInGuess = countMatched(newGuess,char)  // total number of char matched in newGuess
+                wrongPosition += if(totalCharMatchedInSecret==totalCharMatchedInGuess || totalCharMatchedInSecret>totalCharMatchedInGuess) totalCharMatchedInGuess else totalCharMatchedInSecret
+                evaluatedChar.add(char)
+            }
+
+        }
+    }
+    return wrongPosition
+}
+// counting the number of char found in this given string
+fun countMatched(string: String, char: Char): Int {
+    var totalMatches = 0
+    for(i in 0 until string.length){
+        if(string[i]==char)
+        totalMatches++
+    }
+    return totalMatches
+}
---
+++ src/playMastermind.kt
@@ -0,0 +1,68 @@
+package mastermind
+
+import kotlin.random.Random
+// instead of colors we are using A..F letters
+
+val ALPHABET = 'A'..'F'
+const val CODE_LENGTH = 4
+
+fun main() {
+    // for secret letter
+    val differentLetters = false
+    playMastermind(differentLetters)
+}
+
+/**
+ * @param differentLetters: if true> a latter can be repeat in the secret string   else not
+ * @param secret: generated random secret string
+ *
+ * */
+fun playMastermind(
+        differentLetters: Boolean,
+        secret: String = generateSecret(differentLetters)
+) {
+    var evaluation: Evaluation
+
+    do {
+        print("Your guess: ")
+        var guess = readLine()!! // player 2 guess string
+        while (hasErrorsInInput(guess)) {
+            println("Incorrect input: $guess. " +
+                    "It should consist of $CODE_LENGTH characters from $ALPHABET. " +
+                    "Please try again.")
+            guess = readLine()!!
+        }
+        // evaluated result
+        evaluation = evaluateGuess(secret, guess)
+        if (evaluation.isComplete()) {
+            println("The guess is correct!")
+        } else {
+            println("Right positions: ${evaluation.rightPosition}; " +
+                    "wrong positions: ${evaluation.wrongPosition}.")
+        }
+
+    } while (!evaluation.isComplete())
+}
+
+fun Evaluation.isComplete(): Boolean = rightPosition == CODE_LENGTH
+// validation
+fun hasErrorsInInput(guess: String): Boolean {
+    val possibleLetters = ALPHABET.toSet()
+    return guess.length != CODE_LENGTH || guess.any { it !in possibleLetters }
+}
+
+/**
+ * Generating a random secret string   using Random class
+ * */
+fun generateSecret(differentLetters: Boolean): String {
+    val chars = ALPHABET.toMutableList()
+    return buildString {
+        for (i in 1..CODE_LENGTH) {
+            val letter = chars[Random.nextInt(chars.size)]
+            append(letter)
+            if (differentLetters) {
+                chars.remove(letter)
+            }
+        }
+    }
+}
